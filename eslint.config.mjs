3e83e6c6553e717aca353654294d6e01512a5f52
rasel1911 <rasel.ru.eee19@gmail.com>
2025-06-24 13:41:43 +0600

    Refactor ESLint configuration; add prettier and hasky to note; update package.json and pnpm-lock.yaml for new dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,19 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js'
+import { FlatCompat } from '@eslint/eslintrc'
+ 
+const compat = new FlatCompat({
+  // import.meta.dirname is available after Node.js v20.11.0
+  baseDirectory: import.meta.dirname,
+  recommendedConfig: js.configs.recommended,
+})
+ 
 
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
-  tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-]);
+const eslintConfig =  [
+  ...compat.config({
+    extends: ['eslint:recommended', 'next'],
+  }),
+]
+ 
+export default eslintConfig
+
